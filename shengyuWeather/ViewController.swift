7a4240dd1456a33a70f557ef59b615d8a00a88e2
shengyugithub <594549026@qq.com>
2016-05-05 19:48:23 +0800

    Initial Commit

---
+++ shengyuWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shengyuWeather
+//
+//  Created by 盛钰 on 16/5/5.
+//  Copyright © 2016年 shengyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
