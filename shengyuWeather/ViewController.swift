e7992912b1c702b7d6c5acb2b479b2040d2ac356
shengyugithub <594549026@qq.com>
2016-05-12 23:25:34 +0800

    parse json

--- shengyuWeather/ViewController.swift
+++ shengyuWeather/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         NSLog("good")
+        getData()
         //hello world!
     }
 
@@ -22,6 +26,64 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getData(){
+        
+        let defaultSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        var dataTask: NSURLSessionDataTask
+        
 
+     
+        let url = NSURL(string:  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3d5b744314d045c612bda1540a4f503e")
+        dataTask = defaultSession.dataTaskWithURL(url!){
+            data, response, error in
+            dispatch_async(dispatch_get_main_queue()){
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            }
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let httpResponse = response as? NSHTTPURLResponse{
+                if httpResponse.statusCode == 200{
+                    self.parseJSON(data!)
+                }
+            }
+        }
+        
+        dataTask.resume()
+        
+    }
+
+
+    func parseJSON(data :NSData)->[String: AnyObject]?{
+        
+        var json :[String :AnyObject]
+        do {
+            json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions()) as![String :AnyObject]
+            parseDictionary(json)
+            return json
+            }catch{
+                print(error)
+            }
+            return nil
+    }
+    
+    
+    private func parseDictionary(dictionary :[String :AnyObject])-> weatherModel{
+        let weatherResult = weatherModel()
+        //
+        guard let item = dictionary as? [String :AnyObject],
+            let name = item["name"] as? String
+//            let weather = item["weather"] as? [String :AnyObject],
+//            let description = weather["description"]
+//        
+            else{
+                return weatherResult
+            }
+        print("City \(name)'s weather is \(description)")
+        weatherResult.name = name
+       
+        //
+        return weatherResult
+    }
+    
 }
-
---
+++ shengyuWeather/resultModel.swift
@@ -0,0 +1,30 @@
+//
+//  resultModel.swift
+//  shengyuWeather
+//
+//  Created by 盛钰 on 16/5/12.
+//  Copyright © 2016年 shengyu. All rights reserved.
+//
+
+import Foundation
+
+struct coordInfo{
+    var lon = 0.0
+    var lat = 0.0
+}
+
+struct weather {
+    var id = 0
+    var main = ""
+    var description = ""
+    var icon = ""
+}
+
+class weatherModel{
+    var coord : coordInfo
+    var id: Double = 0.0
+    var name: String = ""
+    init(){
+        coord = coordInfo()
+    }
+}
